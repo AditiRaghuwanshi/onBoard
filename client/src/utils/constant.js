ddce425aa39696129bfbeaa6743cf0d1ec87aa03
Aditi <130664278+AditiRaghuwanshi@users.noreply.github.com>
2025-08-24 18:15:21 +0530

    cors connection

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -1,6 +1,7 @@
 
 // const BASE_URL = "http://localhost:8000/api/v1";
-const BASE_URL = "https://onboard-backend.onrender.com";
+// const BASE_URL = "https://onboard-backend.onrender.com";
+const BASE_URL = "https://onboard-backend.onrender.com/api/v1";
 
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
